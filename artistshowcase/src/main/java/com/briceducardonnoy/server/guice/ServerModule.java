072a471d6083702696ca7df2bc0a8218a09e11a1
Brice DUCARDONNOY <brice@ducardonnoy.com>
2014-07-19 12:14:11 +0200

    Remove server part

--- artistshowcase/src/main/java/com/briceducardonnoy/server/guice/ServerModule.java
+++
@@ -1,9 +0,0 @@
-package com.briceducardonnoy.server.guice;
-
-import com.gwtplatform.dispatch.rpc.server.guice.HandlerModule;
-
-public class ServerModule extends HandlerModule {
-    @Override
-    protected void configureHandlers() {
-    }
-}
