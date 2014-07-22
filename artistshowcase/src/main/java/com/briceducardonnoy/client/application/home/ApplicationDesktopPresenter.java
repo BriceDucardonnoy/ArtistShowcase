85dc163eecd3a9db01ce7b3ab79461a9666e83a4
Brice DUCARDONNOY <brice@ducardonnoy.com>
2014-07-22 19:24:22 +0200

    Add i18n support

--- artistshowcase/src/main/java/com/briceducardonnoy/client/application/home/ApplicationDesktopPresenter.java
+++ artistshowcase/src/main/java/com/briceducardonnoy/client/application/home/ApplicationDesktopPresenter.java
@@ -6,12 +6,16 @@ import com.allen_sauer.gwt.log.client.Log;
 import com.briceducardonnoy.client.application.AbstractApplicationPresenter;
 import com.briceducardonnoy.client.application.ApplicationPresenter;
 import com.briceducardonnoy.client.application.ApplicationUiHandlers;
+import com.briceducardonnoy.client.lang.Translate;
+import com.google.gwt.core.client.GWT;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
 
 public class ApplicationDesktopPresenter extends AbstractApplicationPresenter implements ApplicationUiHandlers {
 	@Inject
 	PlaceManager placeManager;
+	
+	private final Translate translate = GWT.create(Translate.class);
 
 	@Inject
 	public ApplicationDesktopPresenter(EventBus eventBus, MyView view, MyProxy proxy) {
@@ -20,6 +24,8 @@ public class ApplicationDesktopPresenter extends AbstractApplicationPresenter im
         view.setUiHandlers(this);
         
         Log.info("Desktop presenter");
+        Log.info(translate.Selection());
+        
     }
 
 }
